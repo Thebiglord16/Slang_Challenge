7dbed157f0aba2ff70845b23afdb526199fe05c5
TheBigLord16 <l.mejia@uniandes.edu.co>
2021-04-21 10:48:32 -0500

    done

--- n-grams.py
+++ n-grams.py
@@ -1,9 +1,17 @@
 
 def calculateNGrams(text, n):
     n_grams = []
-    print(len(text)+1 -n)
     for i in range(len(text)+1 -n):
         n_grams.append(text[i:i+n])
     return n_grams
+    
+def mostFrequentNGram(text, n):
+    n_grams = {}
+    for i in range(len(text)+1 -n):
+        n_gram = text[i:i+n]
+        if n_gram in n_grams:
+            n_grams[n_gram] += 1
+        else:
+            n_grams[n_gram] = 1
+    return max(n_grams, key = n_grams.get)
 
-print(calculateNGrams("to be or not to be", 2))
